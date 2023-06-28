cc630ed0fe38396d0b94888c531159d0e27bf287
Azra Mamic <mamic_azra@hotmail.com>
2023-06-28 19:36:18 +0200

    Initial commit

---
+++ src/main/java/com/example/slmpruefung/SlmPruefungApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmpruefung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmPruefungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmPruefungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slmpruefung/SlmPruefungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmpruefung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmPruefungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
